c0244d738c2c7541dd47b4f9134a6566418461a9
Thaumy <thaumy@outlook.com>
2023-09-20 12:16:02 +0800

    fix: fish processor

--- src/processor/fish.rs
+++ src/processor/fish.rs
@@ -1,30 +1,79 @@
 use regex::Regex;
-use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use std::ops::Not;
 
-#[derive(Serialize, Deserialize)]
+// WRN: fish_history file is not YAML, so we can not use serde
+// Related: https://github.com/fish-shell/fish-shell/issues/4675
 struct Entry {
     pub cmd: String,
     pub when: usize,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub paths: Option<Vec<String>>,
+    pub paths: Option<HashSet<String>>,
+}
+
+fn serialize(entry_vec: Vec<Entry>) -> String {
+    entry_vec
+        .into_iter()
+        .map(|entry| {
+            let mut string = String::new();
+            string.push_str(&format!("- cmd: {}\n", entry.cmd));
+            string.push_str(&format!("  when: {}", entry.when));
+            if let Some(paths) = entry.paths {
+                string.push_str("\n  paths:");
+                paths.into_iter().for_each(|path| {
+                    string.push_str(&format!("\n    - {}", path));
+                });
+            }
+            string
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+fn deserialize(history: &str) -> Vec<Entry> {
+    history.lines().fold(vec![], |mut acc, line| match true {
+        _ if line.starts_with("- cmd: ") => {
+            acc.push(Entry {
+                cmd: line[7..].to_owned(),
+                when: 0,
+                paths: None,
+            });
+            acc
+        }
+        _ if line.starts_with("  when: ") => {
+            acc.last_mut().unwrap().when = str::parse::<usize>(&line[8..])
+                .unwrap_or_else(|e| panic!("parse `when` to usize failed: {}", e));
+            acc
+        }
+        _ if line.starts_with("  paths:") => {
+            acc.last_mut().unwrap().paths = Some(HashSet::new());
+            acc
+        }
+        _ if line.starts_with("    - ") => {
+            acc.last_mut()
+                .unwrap()
+                .paths
+                .as_mut()
+                .expect("`path` is None, unable to insert")
+                .insert(line[6..].to_owned());
+            acc
+        }
+        _ => panic!("Invalid line format: {}", line),
+    })
 }
 
 pub fn filter(history: &str, regex_set: &[Regex]) -> String {
-    let entry_vec: Vec<Entry> =
-        serde_yaml::from_str(history).unwrap_or_else(|_| panic!("Can not deserialize history"));
+    let entry_vec: Vec<Entry> = deserialize(history);
     let entry_vec = entry_vec
         .into_iter()
         .filter(|entry| regex_set.iter().any(|r| r.is_match(&entry.cmd)).not())
         .collect::<Vec<Entry>>();
-    serde_yaml::to_string(&entry_vec).unwrap_or_else(|_| panic!("Can not serialize to history"))
+    serialize(entry_vec)
 }
 
 #[test]
 fn test_filter() {
     let history = r#"- cmd: ll
   when: 1695003484
-- cmd: cargo outdated -R
+- cmd: sudo netstat -anp | awk '$5 == "LISTEN" || $5 == "CONNECTED" {count[$5]++} END {printf "Listen: %d, Conn: $d", count["LISTEN"], count["CONNECTED"]}'
   when: 1695003501
 - cmd: cargo add toml@0.8.0
   when: 1695003525
@@ -44,17 +93,15 @@ fn test_filter() {
     let left = filter(history, &regex_set);
     println!("{}", left);
 
-    // TODO: The format of serde_yaml output is different, but fish works well on it.
     let right = r#"- cmd: ll
   when: 1695003484
-- cmd: cargo outdated -R
+- cmd: sudo netstat -anp | awk '$5 == "LISTEN" || $5 == "CONNECTED" {count[$5]++} END {printf "Listen: %d, Conn: $d", count["LISTEN"], count["CONNECTED"]}'
   when: 1695003501
 - cmd: cargo add toml@0.8.0
   when: 1695003525
 - cmd: vi .local/share/fish/fish_history
   when: 1695044139
   paths:
-  - .local/share/fish/fish_history
-"#;
+    - .local/share/fish/fish_history"#;
     assert_eq!(left, right)
 }
